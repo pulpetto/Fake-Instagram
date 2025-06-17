254a006c936feb5622e02bf64738da8f2a2e24d6
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-17 19:44:30 +0200

    add signin method

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -1,10 +1,32 @@
 import 'package:fake_instagram/features/auth/domain/entities/app_user.dart';
 import 'package:fake_instagram/features/auth/domain/repos/auth_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthRepo implements AuthRepo {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<AppUser?> signInWithEmailAndPassword(String email, String password) {
-    throw UnimplementedError();
+  Future<AppUser?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential userCredential =
+          await firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: userCredential.user!.email!,
+        name: '',
+      );
+
+      return user;
+    } catch (e) {
+      throw Exception('Login failed: $e');
+    }
   }
 
   @override
