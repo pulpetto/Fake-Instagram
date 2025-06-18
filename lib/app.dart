80d9aa3633a80c1acaf239bb27d7c625ae196a03
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-18 12:35:41 +0200

    Displaying correct page on app initialize

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,46 @@
+import 'package:fake_instagram/features/auth/data/firebase_auth_repo.dart';
+import 'package:fake_instagram/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:fake_instagram/features/auth/presentation/cubits/auth_states.dart';
 import 'package:fake_instagram/features/auth/presentation/pages/auth_page.dart';
 import 'package:fake_instagram/themes/light_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final authRepo = FirebaseAuthRepo();
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightMode,
-      home: const AuthPage(),
+    return BlocProvider(
+      create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: lightMode,
+        home: BlocConsumer<AuthCubit, AuthState>(
+          builder: (context, authState) {
+            if (authState is UnAuthenticated) {
+              return const AuthPage();
+            }
+
+            if (authState is Authenticated) {
+              return const HomePage();
+            }
+
+            // loading...
+
+            else {
+              return const Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+          },
+          listener: (context, state) {},
+        ),
+      ),
     );
   }
 }
