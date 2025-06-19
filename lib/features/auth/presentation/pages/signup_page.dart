cc70fd5f072a8958a95b6f21955672417fe82ccf
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-19 05:05:48 +0200

    user signup logic

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,6 +1,8 @@
 import 'package:fake_instagram/features/auth/presentation/components/my_button.dart';
 import 'package:fake_instagram/features/auth/presentation/components/my_text_field.dart';
+import 'package:fake_instagram/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignupPage extends StatefulWidget {
   final void Function()? togglePages;
@@ -15,6 +17,45 @@ class _SignupPageState extends State<SignupPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
+
+  void signup() {
+    final String name = nameController.text;
+    final String email = emailController.text;
+    final String password = passwordController.text;
+    final String confirmPassword = confirmPasswordController.text;
+
+    final authCubit = context.read<AuthCubit>();
+
+    if (name.isNotEmpty &&
+        email.isNotEmpty &&
+        password.isNotEmpty &&
+        confirmPassword.isNotEmpty) {
+      if (password == confirmPassword) {
+        authCubit.signUp(name, email, password);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Passwords do not match'),
+          ),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('All fields are required'),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +105,7 @@ class _SignupPageState extends State<SignupPage> {
                   obscureText: true,
                 ),
                 const SizedBox(height: 25),
-                MyButton(onTap: () {}, text: 'Sign up'),
+                MyButton(onTap: signup, text: 'Sign up'),
                 const SizedBox(height: 50),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
