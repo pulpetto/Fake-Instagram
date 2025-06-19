2c71fa63a44ebb8c5d3e6dd8b16914c871e82676
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-19 08:09:51 +0200

    change folder name & add drawer

---
+++ lib/features/home/presentation/components/my_drawer.dart
@@ -0,0 +1,70 @@
+import 'package:fake_instagram/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:fake_instagram/features/home/presentation/components/my_drawer_tile.dart';
+import 'package:fake_instagram/features/profile/presentation/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              Icon(
+                Icons.person,
+                size: 80,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              Divider(
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              MyDrawerTile(
+                title: 'H o m e',
+                icon: Icons.home,
+                onTap: Navigator.of(context).pop,
+              ),
+              MyDrawerTile(
+                title: 'P r o f i l e',
+                icon: Icons.person,
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ProfilePage(),
+                    ),
+                  );
+                },
+              ),
+              MyDrawerTile(
+                title: 'S e a r c h',
+                icon: Icons.search,
+                onTap: () {},
+              ),
+              MyDrawerTile(
+                title: 'S e t t i n g s',
+                icon: Icons.settings,
+                onTap: () {},
+              ),
+              const Spacer(),
+              MyDrawerTile(
+                title: 'L o g o u t',
+                icon: Icons.logout,
+                onTap: () {
+                  context.read<AuthCubit>().logOut();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,5 @@
-import 'package:fake_instagram/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:fake_instagram/features/home/presentation/components/my_drawer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,15 +14,8 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              context.read<AuthCubit>().logOut();
-            },
-            icon: const Icon(Icons.logout),
-          ),
-        ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
