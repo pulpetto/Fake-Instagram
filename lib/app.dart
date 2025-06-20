de3f4bcda6b812d2ecb61e2e97bbe6b7da940e7e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-20 10:14:53 +0200

    add multi bloc provider

--- lib/app.dart
+++ lib/app.dart
@@ -3,19 +3,29 @@ import 'package:fake_instagram/features/auth/presentation/cubits/auth_cubit.dart
 import 'package:fake_instagram/features/auth/presentation/cubits/auth_states.dart';
 import 'package:fake_instagram/features/auth/presentation/pages/auth_page.dart';
 import 'package:fake_instagram/features/home/presentation/pages/home_page.dart';
+import 'package:fake_instagram/features/profile/data/firebase_profile_repo.dart';
+import 'package:fake_instagram/features/profile/presentation/cubit/profile_cubit.dart';
 import 'package:fake_instagram/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   final authRepo = FirebaseAuthRepo();
+  final profileRepo = FirebaseProfileRepo();
 
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthCubit>(
+          create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
+        ),
+        BlocProvider<ProfileCubit>(
+          create: (context) => ProfileCubit(profileRepo: profileRepo),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: lightMode,
